da494e8b7f5776740f37061077071acdc23d4e52
hafsa002 <hafsabucse3@gmail.com>
2023-06-26 07:33:16 +0600

    layout components are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
+
+    <Layout>
     <h1>Hi</h1>
+    </Layout>
   );
 }
 
